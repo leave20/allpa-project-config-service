a5edd515b21b7aa686bf3de4bc89ac9cd9de1894
ccastrop <ccastrop@everis.com>
2022-09-06 00:00:59 -0500

    Initial commit

---
+++ src/main/java/com/craig/allpaprojectconfigservice/AllpaProjectConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.craig.allpaprojectconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllpaProjectConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllpaProjectConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/craig/allpaprojectconfigservice/AllpaProjectConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craig.allpaprojectconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllpaProjectConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
