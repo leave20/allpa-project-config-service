fa1564418985f2f50d13f5352ac29883d7942558
ccastrop <ccastrop@everis.com>
2022-09-06 21:05:26 -0500

    feat: update

--- src/main/java/com/craig/allpaprojectconfigservice/AllpaProjectConfigServiceApplication.java
+++ src/main/java/com/craig/allpaprojectconfigservice/AllpaProjectConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.craig.allpaprojectconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AllpaProjectConfigServiceApplication {
 
     public static void main(String[] args) {
